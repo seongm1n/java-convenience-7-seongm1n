0d48c785f168a30ab6bcbe9fbb7a6ff566d536d4
seongmin <0411tjdals34@gmail.com>
2024-11-08 22:25:11 +0900

    feat: 결제 가능 여부 확인

--- src/main/java/store/controller/BuyController.java
+++ src/main/java/store/controller/BuyController.java
@@ -10,20 +10,32 @@ import java.util.Map;
 public class BuyController {
 
     public void buyProduct(Store store) {
-        Map<String, Integer> purchasedItems = InputView.purchasedItems();
-        for (String key : purchasedItems.keySet()) {
-            Product product = checkInventory(store, key, purchasedItems.get(key));
-        }
+        while(true) {
+            Map<String, Integer> purchasedItems = InputView.purchasedItems();
+            for (String key : purchasedItems.keySet()) {
+                try {
+                    Product product = checkInventory(store, key, purchasedItems.get(key));
 
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
     }
 
     private Product checkInventory(Store store, String product, int number) {
+        if (product == null || number <= 0) {
+            throw new IllegalArgumentException("[Error] 잘못된 입력입니다. 다시 입력해 주세요.");
+        }
         List<Product> productList = store.getProductList();
         for (Product p : productList) {
-            if (p.getName().equals(product) && p.getQuantity() >= number) {
+            if (p.getName().equals(product) && p.getQuantity() < number) {
+                throw new IllegalArgumentException("[Error] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+            }
+            if (p.getName().equals(product)) {
                 return p;
             }
         }
-        return null;
+        throw new IllegalArgumentException("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.");
     }
 }
